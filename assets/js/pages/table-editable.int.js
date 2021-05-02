52c648ea7ce6ff86950e1abd145a89f35cef3fc6
taiwoj121 <taiwoj121@gmail.com>
2021-05-02 19:16:57 +0100

    boq page

--- assets/js/pages/table-editable.int.js
+++ assets/js/pages/table-editable.int.js
@@ -1,1 +1,5 @@
 $(function(){var e={};$(".table-edits tr").editable({dropdowns:{gender:["Male","Female"]},edit:function(t){$(".edit i",this).removeClass("fa-pencil-alt").addClass("fa-save").attr("title","Save")},save:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in e&&(e[this].destroy(),delete e[this])},cancel:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in e&&(e[this].destroy(),delete e[this])}})});
+
+
+
+$(function(){var f={};$(".table-edits tr").editable({dropdowns:{gender1:["Male","Female","goat"]},edit:function(t){$(".edit i",this).removeClass("fa-pencil-alt").addClass("fa-save").attr("title","Save")},save:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in f&&(f[this].destroy(),delete f[this])},cancel:function(t){$(".edit i",this).removeClass("fa-save").addClass("fa-pencil-alt").attr("title","Edit"),this in f&&(f[this].destroy(),delete f[this])}})});
